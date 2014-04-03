a24cd2fcf808ad4125c32ee21108b06dc4fc9bf7
Thomas Romera <erowlin@Thomass-MacBook-Air.local>
2014-04-03 19:15:47 -0400

    Ok

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 var http    =   require('http');
 
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+	next();
+}
+
+app.use(bodyParser());
+app.use(allowCrossDomain);
 
 var databaseUrl = "myDatabase";
 var collections = ["users"]
@@ -10,5 +22,6 @@ var db = require("mongojs").connect(databaseUrl, collections);
 app.get('/', function(request, response) {
    response.send("Hello World");
 });
+
 
 app.listen(8080);
